1b9e426a6aab18143cf10a8876bce968c5e562fc
sovsianykov <70006293+sovsianykov@users.noreply.github.com>
2021-10-23 01:14:07 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,5 +6,5 @@ const rootReducer = combineReducers({
  orderReducer: orderReducer
 })
 
- export const store = createStore(rootReducer, compose( applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+ export const store = createStore(rootReducer, compose( applyMiddleware(thunk)
+    ))
